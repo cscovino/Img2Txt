d1fe715ca714dde108798da40cac8517c0a54382
Diego Lesertesseur <dlesertesseur@gmail.com>
2019-12-09 22:52:22 -0300

    Update app.py

--- service/app.py
+++ service/app.py
@@ -4,5 +4,8 @@ UPLOAD_FOLDER = 'C:/work/ia/curso/TP03/process/images'
 
 app = Flask(__name__)
 app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.debug = False
+
+app.config["LOCATION_IMAGES"] = "../process/location/"
+app.config["TO_PROCESS_IMAGES"] = "../process/images/"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
