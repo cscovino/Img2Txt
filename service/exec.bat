b02aae5bda390758014c6e13a8766f8c07b7c8b3
Diego Lesertesseur <dlesertesseur@gmail.com>
2019-12-09 22:52:25 -0300

    Update exec.bat

--- service/exec.bat
+++ service/exec.bat
@@ -1,12 +1,8 @@
 @echo off
-echo localization
 cd ../localization
 python localization.py
-echo crop
 cd ../crop
 python cropDir.py
-echo transcript
 cd ../transcript
 python transcript.py
 cd service
-echo fin
